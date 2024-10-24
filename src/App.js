fbab98dec2147a105347e97ab0adc897eb6eed3f
AkashBarik07 <122816598+AkashBarik07@users.noreply.github.com>
2024-10-24 13:42:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import DragAndDrops from "./Components/DragAndDrops";
+import "./styles.css";
+
+const initialData = {
+  Todo: [
+    "Design UI mockups",
+    "Set up project repository",
+    "Write unit test",
+    "Integrate payment gateway",
+  ],
+  "In Progress": ["Develop authentication flow", "Implement responsive design"],
+  Completed: [
+    "Set up CI/CD pipeline",
+    "Conduct code reviews",
+    "Deploy initial version to staging",
+  ],
+};
+
+export default function App() {
+  return <DragAndDrops initialState={initialData} />;
+}
